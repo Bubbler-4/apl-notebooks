6bdd3f675810ae96205fe6fa6e234baf5ead6b62
Bubbler-4 <bubbler9903@gmail.com>
2020-08-10 03:55:46 +0000

    Add HTML version of notebooks
    Change directory structure
    Add bash scripts for converting notebooks

---
+++ gen_html.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+pygments_apl_lexer=~/.pyenv/versions/3.8.3/lib/python3.8/site-packages/pygments/lexers/apl.py
+sed -i -e 's/∘]/∘⌸\&⌶@⌺⍥⍛⍢]/' $pygments_apl_lexer
+sed -i -e 's/↗]/↗⊆⊇⍸√⌾…⍮]/' $pygments_apl_lexer
+find . \( ! -path '*/.*' \) -name *.ipynb | while read filename; do
+    jupyter nbconvert $filename --output-dir $(echo $(dirname $filename) | sed "s/^..notebooks/.\/html/") && \
+    sed -i -e 's/"custom\.css"/"\/custom.css"/' $(echo $filename | sed "s/^..notebooks/.\/html/" | sed "s/ipynb$/html/")
+    done
